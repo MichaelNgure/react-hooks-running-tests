1f590ae5363d8caf0b10d0c8ef3e3be005df6c0f
MichaelNgure <mikynjoro01@gmail.com>
2024-07-29 17:15:37 +0300

    Added some tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,17 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug(); //Line added
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+
+// Similar test as the one above
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
